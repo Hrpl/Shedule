cf1184f67bc05dafb0fcfed05cf5fcd269a0c649
Hrpl <ponomarevd61@gmail.com>
2024-12-08 00:32:22 +0300

    регистрация сервисов

---
+++ Shedule.API/Extensions/AddServiceExtension.cs
@@ -0,0 +1,29 @@
+﻿using Mapster;
+using MapsterMapper;
+using Shedule.Infrastructure.Services.Implementations;
+using Shedule.Infrastructure.Services.Interfaces;
+using System.Data;
+using System.Reflection;
+
+namespace Shedule.API.Extensions;
+
+public static class AddServiceExtension
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddMapster();
+        services.AddRegisterService();
+    }
+    public static void AddMapster(this IServiceCollection services)
+    {
+        TypeAdapterConfig config = TypeAdapterConfig.GlobalSettings;
+        config.Scan(Assembly.GetExecutingAssembly());
+
+        Mapper mapperConf = new(config);
+        services.AddSingleton<IMapper>(mapperConf);
+    }
+    public static void AddRegisterService(this IServiceCollection services)
+    {
+        services.AddScoped<IDbConnectionManager, DbConnectionManager>();
+    }
+}
---
+++ Shedule.Infrastructure/Services/Implementations/DbConnectionManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using Shedule.Infrastructure.Services.Interfaces;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+
+namespace Shedule.Infrastructure.Services.Implementations;
+
+public class DbConnectionManager : IDbConnectionManager
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<DbConnectionManager> _logger;
+    private string NpgsqlConnectionString => _configuration["ConnectionString:DefaultConnection"];
+
+    public DbConnectionManager(IConfiguration configuration, ILogger<DbConnectionManager> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    private NpgsqlConnection PostgresDbConnection => new(NpgsqlConnectionString);
+
+    public QueryFactory PostgresQueryFactory => new(PostgresDbConnection, new PostgresCompiler(), 60)
+#if DEBUG
+    {
+        Logger = compiled => { _logger.LogInformation("Query = {@Query}", compiled.ToString()); }
+    }
+#endif
+    ;
+}
